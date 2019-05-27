aa4f56ac2c14365426c8d2e03bc069912fa97276
kakeshita <kakeshitamasaki@gmail.com>
2019-05-27 22:23:20 +0900

    add new file

---
+++ test.py
@@ -0,0 +1,29 @@
+#  文字列の操作
+
+s = 'kojima\naito\nozaki'
+print(s)
+print(len(s))
+
+a = 19/3  # 割り算
+b = 19//3  # 割り捨て
+c = 19%3  # 余り
+d = 3**3  # べき乗
+print(a,b,c,d)
+
+s = r'kojima\naitou\nozaki'
+print(s)
+print(len(s))
+
+a = 'namie'*2+'saikou'
+print(a)
+
+word = "abcDegGhiJK"
+print(word[0])
+print(word[5])
+print(word[2:8])
+print(word[-2])
+print(word[-2:])
+print(word[:-2])
+
+word = 'elephantman'
+print(len(word))
